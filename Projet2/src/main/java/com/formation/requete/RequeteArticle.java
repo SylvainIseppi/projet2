ab164fbb3900e32c4028124b4988eb06e3fa7f20
SylvainIseppi <sylvain.iseppi@gmail.com>
2017-07-26 09:54:29 +0200

    modification requete client + ajout requete article

--- Projet2/src/main/java/com/formation/requete/RequeteArticle.java
+++ Projet2/src/main/java/com/formation/requete/RequeteArticle.java
@@ -4,22 +4,15 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 import com.formation.basesql.ConnexionBdd;
 
-
-public class RequeteClient {
-	static Connection connexion=ConnexionBdd.getInstance();
-	
-	public  static ResultSet getAllClient(){
+public class RequeteArticle {
+	static Connection connexion = ConnexionBdd.getInstance();
+	public  static ResultSet getAllArticle(){
 		try {
 			Statement state= connexion.createStatement();
-			ResultSet resultat= state.executeQuery("select * from client");
+			ResultSet resultat= state.executeQuery("select * from article");
 			/* Exécution d'une requête de lecture */
 	
 
@@ -31,39 +24,10 @@ public class RequeteClient {
 		}
 		return null;
 	}
-	public static  ResulSet getUnClient(int id){
+	public static  ResultSet getUnArticle(int id){
 		try {
 			Statement state= connexion.createStatement();
-			ResultSet resultat= state.executeQuery("select * from client where code="+id);
-			/* Exécution d'une requête de lecture */
-	
-
-			/* Récupération des données du résultat de la requête de lecture */
-			return resultat;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public static  ResultSet AjoutClient(boolean carte,String prenom,String nom,String adresse,int fixe,int mobile,String mail,String remarque){
-		Date date =new Date();
-			
-		try {
-			Statement state= connexion.createStatement();
-			ResultSet resultat= state.executeQuery("INSERT INTO `client`(`dateCreation`, `cartedefidélité`, `prenom`, `nom`, `adresse`, `fixe`, `mobile`, `email`, `remarques`) VALUES ("+new SimpleDateFormat("yyyy-MM-dd").format(date)+","+carte+","+prenom+","+nom+","+adresse+","+fixe+","+mobile+","+mail+","+remarque+") ");
-			/* Exécution d'une requête de lecture */
-			/* Récupération des données du résultat de la requête de lecture */
-			return resultat;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-	public static  ResultSet DelClient(int id){
-		try {
-			Statement state= connexion.createStatement();
-			ResultSet resultat= state.executeQuery("DELETE FROM `client` WHERE code="+id);
+			ResultSet resultat= state.executeQuery("select * from article where id="+id);
 			/* Exécution d'une requête de lecture */
 	
 
@@ -75,17 +39,49 @@ public class RequeteClient {
 		}
 		return null;
 	}
-	public static  void UpdateClient(int id,boolean carte,String prenom,String nom,String adresse,int fixe,int mobile,String mail,String remarque){
-		Date date =new Date();
-			
+	public static  ResultSet delArticle(int id){
 		try {
 			Statement state= connexion.createStatement();
-			ResultSet resultat= state.executeQuery("UPDATE `client` SET `cartedefidélité`="+carte+",`prenom`="+prenom+",`nom`="+nom+",`adresse`="+adresse+",`fixe`="+fixe+",`mobile`="+mobile+",`email`="+mail+",`remarques`="+remarque+" WHERE code="+id+") ");
+			ResultSet resultat= state.executeQuery("DELETE FROM `article` WHERE id="+id);
 			/* Exécution d'une requête de lecture */
+	
+
 			/* Récupération des données du résultat de la requête de lecture */
+			return resultat;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
+	}
+	public static  ResultSet addArticle(int codeCat, String desi,int quantiteStock,int prixUnitaire){
+		try {
+			Statement state= connexion.createStatement();
+			ResultSet resultat= state.executeQuery("INSERT INTO `article`(`codeCategorie`, `designation`, `quantitestock`, `prixUnitaire`) VALUES ("+codeCat+",'"+desi+"',"+quantiteStock+","+prixUnitaire+")");
+			/* Exécution d'une requête de lecture */
+	
+
+			/* Récupération des données du résultat de la requête de lecture */
+			return resultat;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static  ResultSet updateArticle(int codeCat, String desi,int quantiteStock,int prixUnitaire ,int id){
+		try {
+			Statement state= connexion.createStatement();
+			ResultSet resultat= state.executeQuery("UPDATE `article` SET `codeCategorie`="+codeCat+",`designation`='"+desi+"',`quantitestock`="+quantiteStock+",`prixUnitaire`="+prixUnitaire+" WHERE id="+id+")");
+			/* Exécution d'une requête de lecture */
+	
+
+			/* Récupération des données du résultat de la requête de lecture */
+			return resultat;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
